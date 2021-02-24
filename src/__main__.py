84a1bfee6aefa692a8f92d3fe36b253786c993c4
DevinRD <devin.darnell.drd@gmail.com>
2021-02-24 14:58:19 -0700

    slight optimization

--- src/__main__.py
+++ src/__main__.py
@@ -74,6 +74,7 @@ class HP():
                 logger.info('Calling ListenTread.shutdown')
                 listen_thread.shutdown()
 
+        for listen_thread in self.listen_threads:
             while listen_thread.is_alive():
                 time.sleep(.01)
 
@@ -89,6 +90,6 @@ if "__main__" == __name__:
 
     killer = GracefulKiller()
     while not killer.kill_now:
-        time.sleep(5)
+        time.sleep(4)
 
     hp.shutdown()
